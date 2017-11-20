804529639a0bed8f9958183d9ec2cfb8ac4542d2
David Négrier <d.negrier@thecodingmachine.com>
2017-11-20 10:30:02 +0100

    Adding the ability to fetch path of themes from descriptors

--- src/Theme/ThemeDescriptorInterface.php
+++ src/Theme/ThemeDescriptorInterface.php
@@ -6,5 +6,11 @@ namespace TheCodingMachine\CMS\Theme;
 
 interface ThemeDescriptorInterface
 {
-
+    /**
+     * Returns the path of the theme.
+     * This does not have to be an absolute path (it is relative to the theme directory)
+     *
+     * @return string|null
+     */
+    public function getPath(): ?string;
 }
